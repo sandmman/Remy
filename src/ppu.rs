e1e26ed713b7aad340aa2eeabf1aace95da5fec1
Aaron Liberatore <aaron.liberatore@gmail.com>
2016-02-04 15:32:56 -0500

    Initializes cpu registers and separates modules

---
+++ src/ppu.rs
@@ -0,0 +1,11 @@
+struct Ppu {
+    reg_cntrl1: u8,         // address $2000
+    reg_cntrl2: u8,         // address $2001
+    reg_status: u8,         // address $2002
+    reg_spr_ram_address: u8,// address $2003
+    reg_spr_ram_io: u8,     // address $2004
+    reg_vram_address1: u8,  // address $2005
+    reg_vram_address2: u8,  // address $2006
+    reg_vram_io: u8,        // address $2007
+
+}
